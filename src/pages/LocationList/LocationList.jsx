fffd1c192836fd7ecc89b3e1ad5e038d98330a5f
Vanessa Konynenbelt <konyn005@umn.edu>
2022-03-29 17:22:41 -0500

    add card styling

--- src/pages/LocationList/LocationList.jsx
+++ src/pages/LocationList/LocationList.jsx
@@ -6,21 +6,40 @@ function LocationList(props) {
   
   return (
     <>
-    {locations.length?}
-      <h1>Locations</h1>
-        <div>
-          {props.locations?.map(location => (
-            <div key={location._id}>
-              <Link
+      {props.locations.length?
+      <>
+      <h2>Find a new swim spot!</h2>
+        <div className="card-group">
+          {props.locations.map((location) => (
+              <Link 
+                key={location._id}
+                className='App-link'
                 to="/location-page"
-                state={{location}}
-                style={{textDecoration: "none"}}
+                state={{ location }}
               >
-                <h5 className="card-title">Card title: {location.name}</h5> 
-              </Link>
-            </div>
+              <div className="card" style={{ width: '18rem', height: '18rem' }}>
+                {location.pictures?
+                <>
+                  <img className="card-img-top" src={location.pictures} alt="..."></img>
+                </>
+                :
+                <>
+                  <div className="card-body">
+                    <h5 className="card-title">{location.name}</h5>
+                    <p className="card-text">{location.description}</p>
+                  </div>
+                </>
+                }     
+              </div>
+            </Link>
           ))}
         </div>
+      </>
+      :
+      <>
+        <h5>Loading swim spots... </h5> 
+      </>
+      }
     </>
   )
 }
