31afbb611c65fb31b8fe54393803fafd9e87ef04
Vanessa Konynenbelt <konyn005@umn.edu>
2022-03-30 10:12:58 -0500

    adjust links in headers

--- src/components/header.js
+++ src/components/header.js
@@ -4,17 +4,13 @@ import { Link } from 'react-router-dom'
 const Header = ({ user, handleLogout }) => {
      return (
       <>
-      {console.log('this is the user in header', user)}
         <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark" id="mainNav">
           <div className="container px-4 px-lg-5">
           {user ?
              <ul className="navbar-nav ms-auto">
-              <a className="navbar-brand" href="#page-top">Welcome, {user.name}!</a>
-              <li className="nav-item nav-link"><Link to="/profiles">Profiles</Link></li>
-              <li className="nav-item nav-link"><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-              <li className="nav-item nav-link"><Link to="/changePassword">Change Password</Link></li>
-              <li className="nav-item nav-link"><Link to="/add-location">Add Location</Link></li>
-              <li className="nav-item nav-link"><Link to="/locations">All Locations</Link></li>
+                <li className="navbar-item welcome">Welcome, {user.name}!</li>
+                <li className="nav-item"><a className="nav-link" href="/" onClick={handleLogout}>log out</a></li>
+                <li className="nav-item"><a className="nav-link" href="/changePassword">Change Password</a></li>
             </ul>
           :
           <ul className="navbar-nav ms-auto">
@@ -27,9 +23,9 @@ const Header = ({ user, handleLogout }) => {
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ms-auto">
-                    <li className="nav-item"><a className="nav-link" href="#about">About</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#projects">Projects</a></li>
-                    <li className="nav-item"><a className="nav-link" href="#signup">Contact</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/profiles">Swimmers</a></li>
+                    <li className="nav-item"><a className="nav-link" href="/locations">Locations</a></li>
                 </ul>
             </div>
         </div>
