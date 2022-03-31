8f8242d6a0ec8ea868e002de80b7ee12ff39683b
Vanessa Konynenbelt <konyn005@umn.edu>
2022-03-31 12:32:08 -0500

    changed navbar to nav on header

--- src/components/header.js
+++ src/components/header.js
@@ -25,7 +25,6 @@ const Header = ({ user, handleLogout }) => {
                   <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
                   <li className="nav-item"><a className="nav-link" href="/profiles">Swimmers</a></li>
                   <li className="nav-item"><a className="nav-link" href="/locations">Locations</a></li>
-                  <li className="nav-item"><a className="nav-link" href="/add-location">Add Location</a></li>
               </ul>
             </div>
           </div>
